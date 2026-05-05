251e3eda8e41460df3cde97e4ff70eb6b08bbb86
Mayliv <34677589+Mayliv@users.noreply.github.com>
2026-05-05 15:08:54 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    cloudflare(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    react(),
+    tsconfigPaths(),
+  ],
 });
